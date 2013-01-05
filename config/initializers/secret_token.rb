fc4f3c74dbde527bfb55fd9e1bda850ba8d1fe63
t1una <totugekiluna@gmail.com>
2013-01-06 04:58:34 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dartsmaps::Application.config.secret_token = 'b40e2043186950549d5b59deb8fb0b7a10720534d269cbbc22f46e48776cd020859a66404e21f184a4b5a9bb3f0f53d169cf098a246cb784333a092d12cf00e3'
