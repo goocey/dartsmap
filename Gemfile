c1c48a6546fcbffb9b7873338a27677f2448ee0b
t1una <totugekiluna@gmail.com>
2013-01-15 04:17:36 +0900

    commit

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.10'
 
 gem 'sqlite3'
 gem 'oauth2'
+gem 'execjs'
+gem 'therubyracer'
 
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -51,10 +51,6 @@ Dartsmaps::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'sitetop'
-
-
-  match ':action', :controller => :about
 
   # See how all your routes lay out with "rake routes"
 
